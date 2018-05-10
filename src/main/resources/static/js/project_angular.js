6bb8ca0ae737b4f69b93b8fbf1ee505c52561c53
Amit Bist <Amit.Bist@docusign.com>
2018-05-10 01:43:48 -0500

    JS Fix ng-click2

--- src/main/resources/static/js/project_angular.js
+++ src/main/resources/static/js/project_angular.js
@@ -13,6 +13,26 @@ app.controller('ModalCtrl', function($scope, $http, $timeout, DTOptionsBuilder) 
         .withOption('lengthMenu', [5, 10, 50, 100]);
 
 	$scope.roomId = 0;
+	
+	$scope.openRecipientView = function(envelopeId, recipientEmail){
+
+	    	console.log('openRecipientView envelopeId- '
+					+ envelopeId + ' recipientEmail- '
+					+ recipientEmail);
+			$.ajax({
+				type : 'POST',
+				crossOrigin : true,
+				dataType : "json",
+				url : 'createRecipientViewUrl',
+				data : ({
+					envelopeId : envelopeId,
+					recipientEmail : recipientEmail
+				}),
+				success : function(data) {
+					console.log('success redirect- ');
+				}
+			});
+		}
 
 	$scope.fetchAllEnvelopesJSON = function(recipientEmail) {
 
